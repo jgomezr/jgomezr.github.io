efa850b82f3bcf5d20447c18b951fba9e70f7d6f
Julián Gómez <jgomez@san.eco>
2026-07-06 21:53:49 -0500

    Quitar feedApp y workflow PWA; restaurar sitio Jekyll

--- feedApp/app/src/db/index.ts
+++
@@ -1,5 +0,0 @@
-import { dbClient } from './client'
-import { makeRepo } from './repo'
-
-/** Repositorio único de la app, sobre el worker sqlite-wasm (OPFS). */
-export const repo = makeRepo(dbClient)
